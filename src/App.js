f41dcd0f03c81a3f20c180f36ee676f81c9d37a8
unknown <juesgape11@gmail.com>
2023-02-04 07:17:24 -0500

    Replacing each element for different components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+/* import './App.css'; */
+import React from 'react';
+import { TodoCounter } from './components/TodoCounter';
+import { TodoSearch } from './components/TodoSearch';
+import {TodoList} from './components/TodoList';
+import { TodoButton } from './components/TodoButton';
+
+export const todos = [
+  {text: 'Cortar patatas', completed: false},
+  {text: 'Tomar el curso de intro a React', completed: false},
+  {text: 'Llorar', completed: false},
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+
+      <TodoCounter />
+
+      <TodoSearch />
+
+      <TodoList/>
+
+      <TodoButton/>
+
+    </React.Fragment>
   );
 }
 
