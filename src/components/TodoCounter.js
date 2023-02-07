0e3ce918d002f5a86922a6e37efed0dcef17835a
unknown <juesgape11@gmail.com>
2023-02-07 05:29:58 -0500

    Styling components and adding icons

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -3,7 +3,15 @@ import "../styles/TodoCounter.css"
 
 function TodoCounter() {
     return(
-        <h2 className="TodoCounter">You have completed 2 out of 3 tasks </h2> 
+        <React.Fragment>
+
+        <h1 className="TodoCounter">To do list </h1> 
+
+        <div className="todo-span-container">
+            <span className="todo-span">You have completed <span className="tasksCompleted">2</span> out of <span className="total-tasks">3</span> tasks</span>
+        </div>
+
+        </React.Fragment>
     )
 }
 
--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,10 +1,14 @@
 import React from "react";
 import '../styles/TodoSearch.css'
+import {FaSearch} from 'react-icons/fa'
 
 function TodoSearch() {
     return(
-        <div className="search-todo-container">
-            <input className="search-todo" placeholder='Patata'></input>
+        <div className="search-todo-main-container">
+            <div className="search-todo-container">
+                <input className="search-todo" placeholder='Buscar...'></input>
+                <FaSearch className="search-icon"></FaSearch>
+            </div>
         </div>
     )
 }
