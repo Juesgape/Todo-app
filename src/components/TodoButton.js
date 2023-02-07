dff452d78a0c11c6027c31e6cc31e1d5f4fa2080
unknown <juesgape11@gmail.com>
2023-02-07 09:55:38 -0500

    Adding function onclick

--- src/components/TodoButton.js
+++ src/components/TodoButton.js
@@ -2,8 +2,17 @@ import React from "react";
 import '../styles/TodoButton.css'
 
 function TodoButton() {
+    const onClickButton = (msg) => {
+        console.log(msg);
+    }
+
     return(
-            <button className="add-button">+</button>
+            <button className="add-button"
+            onClick={() => onClickButton('Jejeje')}
+            >
+                +
+                
+            </button>
     )
 }
 
