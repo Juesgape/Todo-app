743dedb9927632d48a0ec10a7170c372de328c76
unknown <juesgape11@gmail.com>
2023-02-07 09:56:44 -0500

    Adding complete and delete function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,12 +4,26 @@ import {FaRegTimesCircle} from 'react-icons/fa'
 import {FaRegThumbsUp} from 'react-icons/fa'
 
 function TodoItem(props) {
+    const onComplete = () => {
+        alert('Has completado la tarea!')
+    }
+
+    const onDelete = () => {
+        alert('Tarea borrado exitosamente!')
+    }
+
     return(
         <li className="todo-items">
             <div>
-                <FaRegThumbsUp className="completedTask"></FaRegThumbsUp>
+                <FaRegThumbsUp className="completedTask"
+                onClick={onComplete}
+                />
+
                 <p>{props.text}</p>
-                <FaRegTimesCircle className="deleteTask"></FaRegTimesCircle>
+
+                <FaRegTimesCircle className="deleteTask"
+                onClick={onDelete}
+                />
             </div>
         </li>
     )
