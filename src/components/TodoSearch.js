f2f410d6c2cb05c3144a25acfaf50079349fe174
unknown <juesgape11@gmail.com>
2023-02-07 09:57:36 -0500

    Adding function to the search bat

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -3,10 +3,18 @@ import '../styles/TodoSearch.css'
 import {FaSearch} from 'react-icons/fa'
 
 function TodoSearch() {
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+    }
+
     return(
         <div className="search-todo-main-container">
             <div className="search-todo-container">
-                <input className="search-todo" placeholder='Buscar...'></input>
+
+                <input className="search-todo" placeholder='Buscar...'
+                onChange={onSearchValueChange}
+                />
+                
                 <FaSearch className="search-icon"></FaSearch>
             </div>
         </div>
