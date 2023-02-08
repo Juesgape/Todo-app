8c3492c2d167bc2a5deabb0b4487bbda9f350769
unknown <juesgape11@gmail.com>
2023-02-08 05:50:00 -0500

    Implementing state in the todo search

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -3,22 +3,27 @@ import '../styles/TodoSearch.css'
 import {FaSearch} from 'react-icons/fa'
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('')
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value)
     }
 
-    return(
+    return[
         <div className="search-todo-main-container">
             <div className="search-todo-container">
 
                 <input className="search-todo" placeholder='Buscar...'
+                value={searchValue}
                 onChange={onSearchValueChange}
                 />
                 
                 <FaSearch className="search-icon"></FaSearch>
             </div>
-        </div>
-    )
+        </div>,
+        <p>{searchValue}</p>
+    ]
 }
 
 export {TodoSearch}
