b765423a6b6b2599517ec0e415a6097270331270
unknown <juesgape11@gmail.com>
2023-02-20 15:29:08 -0500

    Deleting unnecessary todos

--- src/App/index.js
+++ src/App/index.js
@@ -3,11 +3,6 @@ import React from 'react';
 import { TodoProvider } from '../components/TodoContext';
 import { AppUI } from './AppUI';
 
-/* export const defaultTodos = [
-  {text: 'Cortar patatas', completed: false},
-  {text: 'Jugar futebol como Messias', completed: false},
-  {text: 'Llorar', completed: false},
-]; */
 function App() {
   return (
     <TodoProvider>
